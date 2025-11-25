e1a00ab82a47086c5aab49bda33b140b179df05d
Rasel66 <raselhossain5437@gmail.com>
2025-11-25 18:16:55 +0600

    content change

--- innovation_hub/settings.py
+++ innovation_hub/settings.py
@@ -66,6 +66,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'innovation_hub',
+        # 'USER': 'root',
+        # 'PASSWORD': '1234',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
